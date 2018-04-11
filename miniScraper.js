5bafb4014d679f781fe817d3eb0e3533f1069973
gverni <g.verni@gmail.com>
2018-04-11 22:36:08 +0100

    First working version of mini IMDB scraper

--- miniScraper.js
+++ miniScraper.js
@@ -1,29 +1,39 @@
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
-var found = false 
 
-const urlTypeTv = 'tv'
-const urlTypeTvEpisode = 'ep'
-const urlTypeMovie = 'ft'
+function scrapeTitle (stringToScrape) {
+  var results = /<a.*?>(.*?)<\/a>\s(.*?)\s/.exec(stringToScrape)
+  return results[1] + ' ' + results[2]
+}
 
-var queryType = urlTypeMovie
-var queryTitle = 'The Edge of Seventeen 2016 720p WEB-DL 999MB ShAaNiG mkv'
-//var queryTitle = encodeURI('The Edge of Seventeen 2016')
+async function searchTitleFromString (stringTitle) {
+  // const urlTypeTv = 'tv'
+  // const urlTypeTvEpisode = 'ep'
+  const urlTypeMovie = 'ft'
 
-// var searchUrl = `http://www.imdb.com/find?q=${queryTitle}&ttype=${queryType}&s=tt`
-//console.log('Fetching ' + urlTemplate)
-while (!found && queryTitle.length > 0) {
+  var queryType = urlTypeMovie
+  var queryTitle = stringTitle.replace(/[^a-zA-z0-9]/g, ' ')
+  var result = ''
+  while (!result && queryTitle.length > 0) {
     console.log('Searching ' + queryTitle)
     let searchUrl = encodeURI(`http://www.imdb.com/find?q=${queryTitle}&ttype=${queryType}&s=tt`)
     await fetch(searchUrl)
-    .then(response => response.text())
-    .then(responseHtml => {
-      const $ = cheerio.load(responseHtml)
-      //console.log($.html())
-      if ($('.findResult')) {
-          found = true 
-          console.log($('.findResult').html())
-      }
-    })
+      .then(response => response.text())
+      .then(responseHtml => {
+        const $ = cheerio.load(responseHtml)
+        if ($('.findResult').length) {
+          result = $('.findResult .result_text').html()
+        }
+      })
     queryTitle = queryTitle.split(' ').slice(0, -1).join(' ')
+  }
+  return result ? Promise.resolve(result) : Promise.reject('No result found')
 }
+
+searchTitleFromString('The.Edge.of.Seventeen.2016.720p.WEB-DL.999MB.ShAaNiG.mkv')
+  .then(result => {
+    console.log('Found ' + scrapeTitle(result))
+  })
+  .catch((err) => {
+    console.log('Error: ' + err)
+  })
